46e091442f0de6d52406c9089f73a5b49485b649
xiaoliang <xiaoliang@tezign.com>
2022-06-27 15:28:10 +0800

    chore: add plugin

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,10 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import commonjs from "vite-plugin-commonjs"
+import { viteCommonjs } from '@originjs/vite-plugin-commonjs'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), commonjs()],
+  plugins: [react(), viteCommonjs()],
   esbuild: {
     jsxFactory: "h",
     jsxFragment: "Fragment",
